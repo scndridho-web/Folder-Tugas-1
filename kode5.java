f95981be203eaafd5eebc7e054d3673a0f1ac953
Ridho <scndridho@gmail.com>
2026-04-05 22:11:15 +0800

    Add files via upload

---
+++ kode5.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;  
+import java.io.IOException;  
+import java.io.InputStreamReader;
+
+public class Bacakar { 
+public static void main(String[] args) throws IOException { 
+char cc; 
+int bil; 
+
+BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in)); 
+
+System.out.print("hello\n"); 
+System.out.print("baca 1 karakter : "); 
+cc = dataIn.readLine().charAt(0); 
+System.out.print("baca 1 bilangan : "); 
+bil = Integer.parseInt(dataIn.readLine()); 
+
+System.out.print(cc + "\n" + bil + "\n"); 
+System.out.print("bye \n"); 
+} 
+}
